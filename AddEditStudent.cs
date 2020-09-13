54470e5e3262cee25d980914bdf7cdf02fd94072
JarekAmilo <yeti_p@gazeta.pl>
2020-09-13 15:25:21 +0200

    add project files

---
+++ AddEditStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentsDiary
+{
+    public partial class AddEditStudent : Form
+    {
+        public AddEditStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
